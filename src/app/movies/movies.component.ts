169a447582a571d24f417a8e1ee60cea379c5994
Utku Demir <utkudemir@github.com>
2020-03-24 13:22:55 +0300

    Lesson 1: Generating Component

---
+++ src/app/movies/movies.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector:'movies',//<div class="movies"></"div>
+    templateUrl:'movies.component.html'
+})
+export class MoviesComponent
+{
+
+}
