7767785ba7c4c7d77f38ff3e7e986fc73cb9b770
Utku Demir <https://github.com/utkudemr>
2020-03-25 10:16:48 +0300

    Lesson: Çift yönlü data binding

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Movies } from './movie.datasource';
+import { Movie } from '../movie';
 
 
 @Component({
@@ -10,4 +11,9 @@ export class MoviesComponent
 {
     title='Movie List';
     movies=Movies;
+    selectedMovie:Movie;
+    onSelect(movie:Movie):void
+    {
+        this.selectedMovie=movie;
+    }
 }
